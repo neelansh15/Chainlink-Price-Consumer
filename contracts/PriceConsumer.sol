a81959299aff6c0556dec093f85d8e1eaf73d2ae
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-05-15 03:05:17 +0530

    ft: derived prices

--- contracts/PriceConsumer.sol
+++ contracts/PriceConsumer.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsumer {
-    AggregatorV3Interface internal priceFeed;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
     /**
      * Network: Kovan
@@ -12,7 +12,7 @@ contract PriceConsumer {
      * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
      */
     constructor() {
-        priceFeed = AggregatorV3Interface(
+        ethUsdPriceFeed = AggregatorV3Interface(
             0x9326BFA02ADD2366b30bacB125260Af641031331
         );
     }
@@ -21,11 +21,52 @@ contract PriceConsumer {
         Returns the latest price of ethereum on kovan
      */
     function getLatestPrice() external view returns (int) {
-        (, int price, , , ) = priceFeed.latestRoundData();
+        (, int price, , , ) = ethUsdPriceFeed.latestRoundData();
         return price;
     }
 
     /**
         Get derived price of any currency in reference to another
+    */
+    function getDerivedPrice(
+        address _base,
+        address _quote,
+        uint8 _decimals
+    ) public view returns (int256) {
+        require(
+            _decimals > uint8(0) && _decimals <= uint8(18),
+            "Invalid decimals"
+        );
+
+        uint256 decimals = uint256(10**uint256(_decimals));
+
+        (, int256 basePrice, , , ) = AggregatorV3Interface(_base)
+            .latestRoundData();
+        uint8 baseDecimals = AggregatorV3Interface(_base).decimals();
+        basePrice = scalePrice(basePrice, baseDecimals, _decimals);
+
+        (, int256 quotePrice, , , ) = AggregatorV3Interface(_quote)
+            .latestRoundData();
+        uint8 quoteDecimals = AggregatorV3Interface(_quote).decimals();
+        quotePrice = scalePrice(quotePrice, quoteDecimals, _decimals);
+
+        return (basePrice / quotePrice) * decimals;
+    }
+
+    /**
+        Normalize price according to the decimals value given.
+        Pad to _priceDecimals if less or divide if more
      */
+    function scalePrice(
+        int256 _price,
+        uint8 _priceDecimals,
+        uint8 _decimals
+    ) internal pure returns (int256) {
+        if (_priceDecimals < _decimals) {
+            return _price * int256(10**uint256(_decimals - _priceDecimals));
+        } else if (_priceDecimals > _decimals) {
+            return _price / int256(10**uint256(_priceDecimals - _decimals));
+        }
+        return _price;
+    }
 }
